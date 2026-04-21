7e706946c410f7e338d26f1940f72f46a567343a
BoooSp <22488@students.mrgs.school.nz>
2026-04-22 10:51:13 +1200

    i wanna go home x2

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import tkinter as tk
 import random
 from tkinter import messagebox
 #from PIL import Image, ImageTk
+import tkinter as tk
 
-def main_page(self):
-    for widget in self.winfo_children():
-        widget.destroy()
-#I shall put all the thingy things inside this
-        label = tk.Label(root,text="Stella Verba")
-        label.pack()
-        self.root.geometry("1200x800")
-        self.root.title("StellaVerba")
 
-root = tk.Tk()
-root.mainloop()
+
+
+
+
+top = tkinter.Tk()
+
+
+
+
+top.mainloop()
