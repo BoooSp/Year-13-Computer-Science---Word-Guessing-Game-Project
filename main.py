3850c3321b493d55ae3e4a391fb7ce4e700acc89
BoooSp <22488@students.mrgs.school.nz>
2026-04-29 10:34:55 +1200

    Fixed blank page

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 root = tk.Tk()
+#The title of the game in the bar thingy on top of the normal interface page
 root.title("StellaVerba")
+#The size of my page
 root.geometry("1200x800")
+#Prevent the width or the height being changed by users to prevent any errors from decreasing or increasing size of the screen
 root.resizable(False,False)
 root.mainloop()
