6004ce8f76c02610aadafe9c20557acb80b7750b
BoooSp <22488@students.mrgs.school.nz>
2026-04-30 09:54:27 +1200

    added title and button

--- main.py
+++ main.py
@@ -12,4 +12,6 @@ root.geometry("1200x800")
 #Prevent the width or the height being changed by users to prevent any errors from decreasing or increasing size of the screen
 label = tk.Label(root, text="Stella Verba")
 label.pack()
+button = tk.Button(root, text="Bye", width = 25,command=root.destroy)
+button.pack()
 root.mainloop()
