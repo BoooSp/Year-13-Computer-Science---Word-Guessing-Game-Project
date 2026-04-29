7d274bff4266d806a7c08c6267d747c02dacc566
BoooSp <22488@students.mrgs.school.nz>
2026-04-30 09:50:47 +1200

    fixed the blank page

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import tkinter as tk
+from tkinter import mainloop
+class StellaVerba:
+    def __init__(self, root):
+        self.root = root
+        self.home_page = ()
 root = tk.Tk()
 #The title of the game in the bar thingy on top of the normal interface page
 root.title("StellaVerba")
 #The size of my page
 root.geometry("1200x800")
 #Prevent the width or the height being changed by users to prevent any errors from decreasing or increasing size of the screen
-root.resizable(False,False)
-
+label = tk.Label(root, text="Stella Verba")
+label.pack()
 root.mainloop()
