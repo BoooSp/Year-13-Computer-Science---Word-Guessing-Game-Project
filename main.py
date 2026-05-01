56f95baa0b7fcbbe95a7c794325b7e87086e3012
BoooSp <22488@students.mrgs.school.nz>
2026-05-01 14:59:55 +1200

    moved the button

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import tkinter as tk
+from PIL import ImageTk, Image
+
 from tkinter import mainloop
-class StellaVerba:
-    def __init__(self, root):
-        self.root = root
-        self.home_page = ()
+
 root = tk.Tk()
 #The title of the game in the bar thingy on top of the normal interface page
 root.title("StellaVerba")
 #The size of my page
 root.geometry("1200x800")
 #Prevent the width or the height being changed by users to prevent any errors from decreasing or increasing size of the screen
-label = tk.Label(root, text="Stella Verba")
+root.resizable(False,False)
+label = tk.Label(root, text="Stella Verba", font="Rubik Bubbles")
 label.pack()
 button = tk.Button(root, text="Bye", width = 25,command=root.destroy)
-button.pack()
+button.place(relx=0.425,rely=0.5)
+
+image = ImageTk
 root.mainloop()
