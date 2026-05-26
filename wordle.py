edcc5ba2486220c833b5b3161dfd7240ad57e913
BoooSp <22488@students.mrgs.school.nz>
2026-05-27 11:09:51 +1200

    try to add wordle function

--- wordle.py
+++ wordle.py
@@ -27,6 +27,34 @@ DarkGrey="#2B2B2B"
 
 Alphabet=["ABCDEF","GHIJKL","MNOPQR","STUVWX","YZ"]
 
-guessedletter=pygame.font.Font("Fonts/")
+guessedletter=pygame.font.Font("Fonts/Italianno-Regular.ttf",25)
+notguessedletter=pygame.font.Font("Fonts/Italianno-Regular.ttf",40)
+
+page.fill("white")
+page.blit(bg,bgg)
+pygame.display.update()
+
+letter_x_spacing=85
+letter_y_spacing=12
+lettersize=75
+
+guesses_count=0
+
+guesses=[[]]*6
+correctguess=[]
+correctguesstring=""
+correctletterbgx=110
+
+indicators=[]
+game_result=""
+
+class letter:
+    def __init__(self,text,bg_position):
+        pass
+    def draw(self):
+        pass
+    def delete(self):
+        pass
+
 
 
